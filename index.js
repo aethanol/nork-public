773c07437fe9b9f1be4c871718bd8d7ef133b27a
Ethan Anderson <aethan@uw.edu>
2016-01-05 18:07:18 -0800

    add node package

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
+var readline = require('readline');
 
-/* Your code goes here! */
+var io = readline.createInterface({ //call the interface "io"
+  input: process.stdin, //input comes from the terminal ("standard in")
+  output: process.stdout //output goes to the terminal ("standard out")
+});
+
+var bag = [];
+var health = 100;
+
