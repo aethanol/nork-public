c5ae6c1cc318c20bbd617c2eeb6d6db6f6fe4da7
Ethan Anderson <aethan@uw.edu>
2016-01-12 19:47:54 -0800

    discard attempted oop

--- index.js
+++ index.js
@@ -3,116 +3,60 @@ var world = require('./lib/world.json');
 
 var readline = require('readline');
 
+var inventory = [];
+var health = 100;
+var playing = true;
+var currRoom = world.rooms[0];
+
 var io = readline.createInterface({ //call the interface "io"
     input: process.stdin, //input comes from the terminal ("standard in")
     output: process.stdout //output goes to the terminal ("standard out")
 });
 
 
-class Room{
-    constructor(id, description){
-        this.id = id;
-        this.description = description;
-        this.items = [];
-        this.exits = [];
-    }
+var game = function(world){
+        console.log("Welcome to Nork! gl;hf");
+        console.log("You find yourself in a barren snowy wasteland naked but a broken fitbit and a pair of flip flops");
+        console.log("To the north there is a cabin, to the east is a foreboding forest");
+        io.question("What would you like to do?", input);
 }
 
-
-class Item{
-    constructor(location, id){
-        this.location = location;
-        this.id = id;
-    }
-}
-
-class Headlamp extends Item{
-    constructor(){
-        super();
-    }
-    
-    on(){
+var input = function(answer){
+        var text = answer.toLowerCase().split(" ");
         
+        if(text[0] === "go") {
+            
+           go(text[1]);
+            
+        }else if(text[0] === "use"){
+            use(text[1]);
+            
+        }else if(text[0] === "take"){
+            
+        }else if(text[0] === "drop"){
+            
+        }else if(text[0] === "breif"){
+            this.breif();
+        }
+}
+
+var go = function(direction){
+    if(world.rooms.exits.indexOf(direction) > -1){
+        currRoom = world.rooms[direction];
+    }else{
+        console.log("you can't go that way");
     }
 }
 
-class Phaser extends Item{
-    constructor(){
-        super();
-    }
-    
-    setting(){
-        
-    }
-}
-
-class Pamphlet extends Item{
-    constructor(){
-        super();
-    }
+var take = function(item){
     
 }
-// Game object
-var Game = function(world){
-    this.build(world);
+
+var use = function(item){
+    
 }
 
-// build the world
-Game.prototype.build = function(world){
-    var worldArr = world.rooms;
-
-    for(var i = 0; i < worldArr.length; i++){
-        
-        var newRoom = new Room(worldArr[i].id, worldArr[i].description);
-        
-        
-        // for(var j = 0; j < worldArr[i].exits[j].length; j++){
-        //     newRoom.exits.push(worldArr[i].exits[j]);
-        // }
-               
-    }
-  
-};
-
-// play the game
-Game.prototype.play = function(){
-    var self = this;
-    console.log('Welcome to Nork! gl;hf');
-    console.log('You find yourself in a barren snowy wasteland naked but a broken fitbit and a pair of flip flops');
-    console.log('To the north there is a cabin, to the east is a foreboding forest');
-};
-
-// uses item
-Game.prototype.use = function(item){
-    var self = this;
-};
-
-// takes item
-Game.prototype.take = function(item){
-    
-};
-
-// goes direction
-Game.prototype.go = function(){
-    
-};
-
-// drop item
-Game.prototype.drop = function(){
-    
-};
-
-// gives description of area
-Game.prototype.brief = function(){
-    
-};
-
-// read item
-Game.prototype.read = function(item){
-    
-};
-
-
-var game = new Game(world);
-game.play();
+var drop = function(item){
+    console.log(inventory[0]);
+}
 
